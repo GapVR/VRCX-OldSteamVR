e714be4ff343bc659f5b9d03baec0ae473d8fc17
GapVR <githubgagap@piyo.ru>
2026-09-24 12:51:29 +0800

    remove card default padding

--- src/components/ui/card/Card.vue
+++ src/components/ui/card/Card.vue
@@ -9,7 +9,7 @@
 <template>
     <div
         data-slot="card"
-        :class="cn('bg-card text-card-foreground flex flex-col gap-6 rounded-xl border py-6 shadow-sm', props.class)">
+        :class="cn('bg-card text-card-foreground flex flex-col gap-6 rounded-xl border shadow-sm', props.class)">
         <slot />
     </div>
 </template>
